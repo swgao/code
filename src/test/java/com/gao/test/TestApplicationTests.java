cb1b82555bef1b5f70e39b5a762dec0e26b5572f
swgao <1878441721@qq.com>
2020-08-12 11:53:37 +0800

    第一次提交

---
+++ src/test/java/com/gao/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gao.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
